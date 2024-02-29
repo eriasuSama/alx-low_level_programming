a5146dcea759029f0b288f5a6932be766ecfbf93
Elias <siyabongaelias505@gmail.com>
2024-02-28 17:36:58 -0800

    change the code formatt

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * verifies if acharacter is uppercase or not
- * @c:tested character
- * return: 1 if upper, 0 if not
- */
+*_isupper - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
 
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-	{
-		return (1);
-	}
-	return (0);
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+
+return (0);
 }
