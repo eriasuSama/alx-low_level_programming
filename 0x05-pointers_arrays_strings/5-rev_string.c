979e5d47b5c61216847566b1804843bf8e8e4296
Elias <siyabongaelias505@gmail.com>
2024-04-11 19:42:22 -0700

    fixed 5 rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,46 +1,25 @@
 #include "main.h"
-/**
-*rev_string - Reverse a string
-*@s: String to reverse
-*Return: Nothing
-*/
-void rev_string(char *s)
-{
-int i = 0, length;
-length = _strlen(s) - 1;
-while (length > i)
-{
-swap_char(s + length, s + i);
-i++;
-length--;
-}
-}
 
 /**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns lenght;
+ * rev_string - reverses string s
+ * @s: string to be reversed
+ *
+ * Return: void
  */
-int _strlen(char *s)
-{
-int count, inc;
-inc = 0;
-for (count = 0; s[count] != '\0'; count++)
-inc++;
 
-return (inc);
+void rev_string (char *s)
+{
+    int count = 0, i, temp = 0;
+
+    while (* (s + count) != '\0')
+    {
+        count++;
+    }
+    for (i = 0 ; i <= ( (count - 1) / 2) ; i++)
+    {
+        temp = * (s + i);
+        * (s + i) = * (s + (count - (i + 1)));
+        * (s + (count - (i + 1))) = temp;
+    }
 }
 
-/**
-*swap_char - swap two characters
-*@a: first character
-*@b: second character
-*Return: nothing
-*/
-
-void swap_char(char *a, char *b)
-{
-char tmp = *a;
-*a = *b;
-*b = tmp;
-}
